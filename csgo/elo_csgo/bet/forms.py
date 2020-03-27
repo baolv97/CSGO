2c09d790f52028a2df95fabd065af86beba8fea8
BaoLV-GD <baolv-gd@BaoLV-GD-M.local>
2020-03-27 17:57:18 +0700

    delte crawlvp

--- csgo/elo_csgo/bet/forms.py
+++ csgo/elo_csgo/bet/forms.py
@@ -1,7 +1,5 @@
 from django import forms
 
-from .models import Crawlvp
-
 class crawl_upcomming(forms.Form):
     at = forms.CharField(label='at', max_length=200)
     cp = forms.CharField(label='cp', max_length=200)
