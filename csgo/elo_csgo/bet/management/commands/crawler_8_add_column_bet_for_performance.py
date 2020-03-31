a48dc9f916e624aac63a810874a24330b09bf40c
BaoLV-GD <baolv-gd@BaoLV-GD-M.local>
2020-03-31 09:25:32 +0700

    fix time out 2

--- csgo/elo_csgo/bet/management/commands/crawler_8_add_column_bet_for_performance.py
+++ csgo/elo_csgo/bet/management/commands/crawler_8_add_column_bet_for_performance.py
@@ -9,20 +9,10 @@ class Command(BaseCommand):
         print("Add column result starting...")
 
         performances = Performance.objects.all()
-        match = Match.objects.all()
-        # for i in range(170000, len(performances)):
-        #     print(performances[i].id)
-        #     if not performances[i].match.bet_team_a:
-        #         continue
-        #     if performances[i].team == performances[i].match.team_a:
-        #         performances[i].bet = performances[i].match.bet_team_a
-        #     else:
-        #         performances[i].bet = performances[i].match.bet_team_b
-        #     performances[i].save()
-        #     print(performances[i].id)
         for i in range(150000, len(performances)):
-            match = Match.objects.filter(id=performances[i].match_id).first()
-            print(match.time)
-            performances[i].time = match.time
-            performances[i].save()
+            if performances[i].time is None:
+                match = Match.objects.filter(id=performances[i].match_id).first()
+                print(match.time)
+                performances[i].time = match.time
+                performances[i].save()
         print("Process is was done...")
