3d22467acb1e0a1592e3e37ec10d0c50800c9281
baolv97 <baolv97@gmail.com>
2020-06-03 08:38:11 +0700

    fixbug/crontab

--- csgo/elo_csgo/bet/management/commands/crontab.py
+++ csgo/elo_csgo/bet/management/commands/crontab.py
@@ -5,12 +5,36 @@ from ...views import save_winrate_5e, save_winrate_vp, crawler_over_5etop
 class Command(BaseCommand):
     help = 'auto crawl'
     def handle(self, *args, **options):
-        call_command('crawler_1_cs_go')
-        call_command('crawler_6_get_list_player')
-        call_command('crawler_7_add_column_result')
-        call_command('crawler_8_add_column_bet_for_performance')
-        call_command('crawler_9_train_elo_for_player')
-        call_command('crawler_2_bet')
         crawler_over_5etop()
+        try:
+            call_command('crawler_1_cs_go')
+        except Exception as e:
+            print("fail crawl over htlv")
+
+        try:
+            call_command('crawler_6_get_list_player')
+        except Exception as e:
+            print("fail get list player")
+
+        try:
+            call_command('crawler_7_add_column_result')
+        except Exception as e:
+            print("fail get list result")
+
+        try:
+            call_command('crawler_8_add_column_bet_for_performance')
+        except Exception as e:
+            print("fail get column")
+
+        try:
+            call_command('crawler_9_train_elo_for_player')
+        except Exception as e:
+            print("fail traing elo")
+
+        try:
+            call_command('crawler_2_bet')
+        except Exception as e:
+            print("fail crawl over vpgame")
+
         save_winrate_5e()
         save_winrate_vp()
